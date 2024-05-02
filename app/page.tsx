29a80e973dd3c6e1db8a9bca513ffd8576d6dab5
Kelvin Charles <kelvinc1291@hotmail.com>
2024-05-02 00:23:39 -0300

    fix: teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <div className="bg-red-600 mx-6 p-5">
-    <h1>Hello tailwind</h1>
-   </div>
+    <div className="mx-6 bg-red-600 p-5">
+      <h1>Hello tailwind</h1>
+    </div>
   );
 }
